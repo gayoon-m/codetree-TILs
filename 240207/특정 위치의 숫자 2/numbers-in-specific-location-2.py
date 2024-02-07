640993b6e602330e569fae4bf4984685f664fb6f
gayoon-m <156059144+gayoon-m@users.noreply.github.com>
2024-02-07 16:02:43 +0900

    Title: 특정 위치의 숫자 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240207/특정 위치의 숫자 2/numbers-in-specific-location-2.py
+++ 240207/특정 위치의 숫자 2/numbers-in-specific-location-2.py
@@ -4,6 +4,5 @@ n = int(input())
 numbers = list(map(int, input().split(' ')))
 
 even_sum = sum(numbers[1::2])
-even_avg = even_sum / (n // 2)
 
-print(even_sum, format(even_avg, ".1f"))
+print(even_sum, format(even_sum / (n // 2), ".1f"))
